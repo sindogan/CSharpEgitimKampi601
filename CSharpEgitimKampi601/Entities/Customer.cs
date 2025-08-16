c4e317efc449ebfecef28a9b3026ce16a9ebf894
Sinan Dogan <sinandogan360@gmail.com>
2025-08-16 16:01:13 +0300

    MongoDb Kullanımı1

---
+++ CSharpEgitimKampi601/Entities/Customer.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi601.Entities
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string  CustomerId { get; set; }
+        //mongodb de id string olarak tutuluyo
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string CustomerCity { get; set; }
+        public decimal CustomerBalance { get; set; }
+        public int  CustomerShoppingCount { get; set; }
+
+    }
+}
---
+++ CSharpEgitimKampi601/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace CSharpEgitimKampi601
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCustomerId = new System.Windows.Forms.TextBox();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCustomerSurname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCustomerCity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCustomerBalance = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCustomerShoppingCount = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCustomerCreate = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(45, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Müşteri Id:";
+            // 
+            // txtCustomerId
+            // 
+            this.txtCustomerId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCustomerId.Location = new System.Drawing.Point(139, 49);
+            this.txtCustomerId.Name = "txtCustomerId";
+            this.txtCustomerId.Size = new System.Drawing.Size(208, 27);
+            this.txtCustomerId.TabIndex = 1;
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCustomerName.Location = new System.Drawing.Point(139, 99);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(208, 27);
+            this.txtCustomerName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(34, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Müşteri Adı:";
+            // 
+            // txtCustomerSurname
+            // 
+            this.txtCustomerSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCustomerSurname.Location = new System.Drawing.Point(139, 147);
+            this.txtCustomerSurname.Name = "txtCustomerSurname";
+            this.txtCustomerSurname.Size = new System.Drawing.Size(208, 27);
+            this.txtCustomerSurname.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(8, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Müşteri Soyadı:";
+            // 
+            // txtCustomerCity
+            // 
+            this.txtCustomerCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCustomerCity.Location = new System.Drawing.Point(139, 203);
+            this.txtCustomerCity.Name = "txtCustomerCity";
+            this.txtCustomerCity.Size = new System.Drawing.Size(208, 27);
+            this.txtCustomerCity.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(19, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Müşteri Şehir:";
+            // 
+            // txtCustomerBalance
+            // 
+            this.txtCustomerBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCustomerBalance.Location = new System.Drawing.Point(139, 260);
+            this.txtCustomerBalance.Name = "txtCustomerBalance";
+            this.txtCustomerBalance.Size = new System.Drawing.Size(208, 27);
+            this.txtCustomerBalance.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(69, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Bakiye:";
+            // 
+            // txtCustomerShoppingCount
+            // 
+            this.txtCustomerShoppingCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCustomerShoppingCount.Location = new System.Drawing.Point(139, 315);
+            this.txtCustomerShoppingCount.Name = "txtCustomerShoppingCount";
+            this.txtCustomerShoppingCount.Size = new System.Drawing.Size(208, 27);
+            this.txtCustomerShoppingCount.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(5, 318);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Alışveriş Sayısı:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(139, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 45);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Listele";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnCustomerCreate
+            // 
+            this.btnCustomerCreate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnCustomerCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCustomerCreate.Location = new System.Drawing.Point(139, 416);
+            this.btnCustomerCreate.Name = "btnCustomerCreate";
+            this.btnCustomerCreate.Size = new System.Drawing.Size(208, 45);
+            this.btnCustomerCreate.TabIndex = 13;
+            this.btnCustomerCreate.Text = "Ekle";
+            this.btnCustomerCreate.UseVisualStyleBackColor = false;
+            this.btnCustomerCreate.Click += new System.EventHandler(this.btnCustomerCreate_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(139, 467);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(208, 45);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(139, 518);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(208, 45);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Güncelle";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(369, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(788, 511);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1199, 701);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnCustomerCreate);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCustomerShoppingCount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCustomerBalance);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCustomerCity);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCustomerSurname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCustomerId);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCustomerId;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCustomerSurname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCustomerCity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCustomerBalance;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCustomerShoppingCount;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCustomerCreate;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
